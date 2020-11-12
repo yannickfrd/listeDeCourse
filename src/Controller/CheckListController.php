c629b7c6e435d4228e69aab7feb89b6145f0cb69
yannick <y.farade@hotmail.fr>
2020-11-12 22:03:05 +0100

    checklist.all in index

--- src/Controller/CheckListController.php
+++ src/Controller/CheckListController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\CheckListFormType;
+use App\Repository\CheckListRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,12 @@ class CheckListController extends AbstractController
     /**
      * @Route("/check/list", name="check_list")
      */
-    public function index(): Response
+    public function index(CheckListRepository $checkListRepository): Response
     {
         $checklistForm = $this->createForm(CheckListFormType::class);
 
         return $this->render('check_list/index.html.twig', [
+            'checklists' => $checkListRepository->findAll(),
             'checklistForm' => $checklistForm->createView()
         ]);
     }
